3ecf46b3c690340b6c963744f3dc1c9814f839ee
salma <jabirisalma10@gmail.com>
2024-11-30 23:03:02 +0100

    creation des messages (and up date controller)

--- Flight-search/server/src/main/java/com/myflights/flight/controller/AppController.java
+++ Flight-search/server/src/main/java/com/myflights/flight/controller/AppController.java
@@ -1,36 +1,43 @@
 package com.myflights.flight.controller;
-
+//import java.awt.TrayIcon;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myflights.flight.entity.Flight;
+import com.myflights.flight.reponse.ObjectReponse;
 import com.myflights.flight.service.FlightService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-
+//import io.micrometer.core.ipc.http.HttpSender;
 @RestController
 public class AppController {
     @Autowired
     private FlightService flightService;
     @GetMapping("/find-flights")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
-    }
+
+
+    public ResponseEntity<ObjectReponse>findFlights(@RequestParam() String origin, @RequestParam String dest, @RequestParam() String date) {
     
-    public List<Flight>findFlights(){
+        ObjectReponse resp = new ObjectReponse();
+    
+
+   // public List<Flight>findFlights(@RequestParam() String origin, @RequestParam() String dest, @RequestParam() String date){
         try {
-            List<Flight> flights = flightService.findFlightsByDate(origin:null, dest:null , date :null);
-            
+            List<Flight> flights = flightService.findFlightsByDate( origin,dest,date );
+            resp.setResponseData(flights);
+            resp.setStatusCode(HttpStatus.OK);
+            resp.setMessageType("SUCCESS");
         } catch (Exception ex) {
+            resp.setStatusCode(HttpStatus.BAD_REQUEST);
+            resp.setMessage("Error occured on the server");
+            resp.setMessageType("ERROR");
             ex.printStackTrace();
         }
-    
-
-     return null;
-
-    }
-    
+        return new ResponseEntity<ObjectReponse>(resp, resp.getStatusCode());
+    }  
 }
---
+++ Flight-search/server/src/main/java/com/myflights/flight/reponse/ObjectReponse.java
@@ -0,0 +1,14 @@
+package com.myflights.flight.reponse;
+
+public class ObjectReponse extends AbstractReponse {
+    private Object responseData;
+
+    public Object getResponseData() {
+        return responseData;
+    }
+
+    public void setResponseData(Object responseData) {
+        this.responseData = responseData;
+    }
+    
+}
